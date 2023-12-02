0434fa1e5a8234d285ff2b3643e90d5ec0236782
Brett Baker <GitBaBB@MyBaBB.com>
2023-12-02 08:05:03 -0800

    about to push to git hub pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css' 
+
 
 function App() {
   const [count, setCount] = useState(0)
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <p className='creepsterText text-4xl text-varLIGHTBLUEFEATHER '>Brett's First Logical Git hub Deploy for Vite React js.</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
